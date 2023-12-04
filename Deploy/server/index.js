1b7b2f2ffe1a55440d5c4b367cc3ad97095d7bda
Hsing-Tzu <daisyko1234@gmail.com>
2023-12-04 14:15:40 +0800

    Update index.js

--- Deploy/server/index.js
+++ Deploy/server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 // Serve static files from the React build directory
-app.use(express.static('./client/build'));
+app.use(express.static('../client/build'));
 
 // For any other route, serve the React app's index.html
 app.get("*", (req, res) => {
